7f5a5678394cd73a2da34345d6e61e4a2d409ef8
Thrynk <thocarin@gmail.com>
2019-09-18 11:04:24 +0200

    test port 80

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.port || 8080, function(){
+app.listen(process.env.PORT || 80, function(){
     console.log("server listening");
 });
