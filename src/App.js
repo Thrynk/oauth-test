67bfa340e14c0dc5a82f5102c9af9199ff0d510b
Thrynk <thocarin@gmail.com>
2019-09-18 10:41:19 +0200

    Test oauth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import config from "./config.json";
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isAuthenticated: false,
+            user: null,
+            token: ''
+        };
+    }
+
+    logout = () => {
+        this.setState({isAuthenticated: false, token: '', user: null});
+    }
+
+    facebookResponse = (response) => {
+        console.log(response)
+        const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+        const options = {
+            method: 'POST',
+            body: tokenBlob,
+            mode: 'cors',
+            cache: 'default'
+        };
+        fetch('http://localhost:4000/api/v1/auth/facebook', options).then(r => {
+            const token = r.headers.get('x-auth-token');
+            r.json().then(user => {
+                if (token) {
+                    this.setState({isAuthenticated: true, user, token})
+                }
+            });
+        })
+    }
+
+    onFailure = (error) => {
+        alert(error);
+    }
+
+    render() {
+        let content = !!this.state.isAuthenticated ?
+            (
+                <div>
+                    <p>Authenticated</p>
+                    <div>
+                        {this.state.user.email}
+                    </div>
+                    <div>
+                        <button
+                            onClick={this.logout}
+                            className="button"
+                        >
+                            Log out
+                        </button>
+                    </div>
+                </div>
+            ) :
+            (
+                <div>
+                    <FacebookLogin
+                        appId={config.FACEBOOK_APP_ID}
+                        autoLoad={false}
+                        fields="name,email,picture"
+                        callback={this.facebookResponse} />
+                </div>
+            );
+
+        return (
+            <div className="App">
+                {content}
+            </div>
+        );
+    }
 }
 
 export default App;
